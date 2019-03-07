ac6fb601b155942957f1b1abc04fcddd556fdeb9
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-07 21:06:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,5 +29,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array
 }
 function removeElementFromEndOfArray(array){
-array.slice(0, array.length - 1)
+return array.slice(0, array.length - 1)
 }
