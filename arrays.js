7633a665423949ef422d737bdb30e8f3570d7e41
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-07 18:49:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-var addElementToBeginningOfArray = ["array"]
-addElementToBeginningOfArray.unshift("foo")
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
