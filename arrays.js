1e87fa320dfb6c3479c8c19f99c008ce44ccc213
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-07 19:06:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
-  return [element, ...array];
+  return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element);
+  array.unshift(element)
   return array
 }
