9e9dc0a43f2ddf9722f124762141070c657f69e5
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-07 20:14:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,6 +22,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array
 }
 function removeElementFromBeginningOfArray(array){
-  array.slice[0]
-  return array
+  return array.slice(1)
 }
