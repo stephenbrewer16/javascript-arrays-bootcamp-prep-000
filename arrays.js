d66ee3a5f6ec65f826a4da366bd0ed02517f1857
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-07 19:40:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
   return array
 }
-function accessElementInArray(array, [index]){
-  return array [index]
+function accessElementInArray(array, index){
+  return array[index]
 }
